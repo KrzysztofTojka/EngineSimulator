6c18db9377b04010372b51c81464a237818814d7
Krzysztof Tojka <tojkakrzysztof@gmail.com>
2026-04-03 15:52:24 +0200

    Audio engine - play wav files

--- EngineSoundTest/synthesis.cpp
+++ EngineSoundTest/synthesis.cpp
@@ -4,6 +4,30 @@
 #include <iostream>
 #include <vector>
 
+struct Audio {
+    std::vector<float> samples = {};
+    int sampleCount = 0;
+    int cursor = 0;
+};
+
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
+    float* outputF32 = (float*) pOutput;
+    Audio* audio = (Audio*) pDevice->pUserData;
+
+    for (ma_uint32 i = 0; i < frameCount; i++) {
+        float sample = audio->samples[audio->cursor];
+
+        outputF32[i * 2] = sample; // left
+        outputF32[i * 2 + 1] = sample; // right
+
+        audio->cursor++;
+
+        if (audio->cursor >= audio->sampleCount) {
+            audio->cursor = 0;
+        }
+    }
+}
+
 bool loadWav(const std::string& filePath, std::vector<float>& out) {
     ma_decoder decoder;
     ma_decoder_config config = ma_decoder_config_init(ma_format_f32, 1, 44100);
@@ -27,12 +51,27 @@ bool loadWav(const std::string& filePath, std::vector<float>& out) {
 
 int main() {
     std::string file = "assets/3000.wav";
-    std::vector<float> samples;
+    Audio audio;
 
-    if (!loadWav(file, samples)) {
+    if (!loadWav(file, audio.samples)) {
         std::cout << "Could not load file " << file << std::endl;
         return -1;
     }
 
-    std::cout << "Loaded " << file << " (" << samples.size() << " samples)" << std::endl;
+    audio.sampleCount = audio.samples.size();
+
+    std::cout << "Loaded " << file << " (" << audio.samples.size() << " samples)" << std::endl;
+
+    ma_device_config config = ma_device_config_init(ma_device_type_playback);
+    config.playback.format = ma_format_f32;
+    config.playback.channels = 2;
+    config.sampleRate = 44100;
+    config.dataCallback = data_callback;
+    config.pUserData = &audio;
+
+    ma_device device;
+    ma_device_init(NULL, &config, &device);
+    ma_device_start(&device);
+
+    std::cin.get();
 }
